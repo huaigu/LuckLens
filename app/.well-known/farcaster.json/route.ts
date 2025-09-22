bb93d61824082622a6ad22627fd7a6c5c2e2436a
bojack <bojack@example.com>
2025-09-22 10:47:13 +0800

    update farcaster.json

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,28 +3,30 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjE3OTc5LCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4MGMxNWE5QkVmRTg3RjY0N0IwMDNhMjI0MTY4NDYwMzYyODQ0M2Y4YiJ9",
-      payload:
-        "eyJkb21haW4iOiJtb25hZC1taW5pYXBwLXRlbXBsYXRlLXNldmVuLnZlcmNlbC5hcHAifQ",
-      signature:
-        "MHgwYzY2NDdjZDhjOWJiY2JmYzg2NGIzZjVjYWVjY2ExMTdlOTY4ZGQwMWIzMmM0NGViMjU5ZDhlOGQyMzdhZTZiMDU1MmNmNWRiMDU1MDMwNTZmNTNhZmEwZDZlZTBlZmIyMmJmNDNmMDQ4NTdhMzk2NmY0YmMzODk2N2NlZDI5ZjFi",
+    "frame": {
+      "name": "LuckLens",
+      "version": "1",
+      "iconUrl": "https://cryptofortune.memego.ai/images/icon.png",
+      "homeUrl": "https://cryptofortune.memego.ai",
+      "buttonTitle": "Draw Fortune",
+      "splashImageUrl": "https://cryptofortune.memego.ai/images/splash.png",
+      "splashBackgroundColor": "#ffffff",
+      "webhookUrl": "https://cryptofortune.memego.ai/api/webhook",
+      "subtitle": "Crypto Fortune",
+      "description": "draw today's trading fortune and get your own cyber proverb",
+      "primaryCategory": "social",
+      "tags": [
+        "luck",
+        "monad",
+        "social",
+        "nft"
+      ]
     },
-    frame: {
-      version: "1",
-      name: "Monad Farcaster MiniApp Template",
-      iconUrl: `${APP_URL}/images/icon.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
-      screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
-    },
+    "accountAssociation": {
+      "header": "eyJmaWQiOjI1MjI5MywidHlwZSI6ImF1dGgiLCJrZXkiOiIweGRlY0E2MjExMGYwOGQ5RDIxN0U5MTlFMDY5QWVkMWFBNDM2YjBkMjYifQ",
+      "payload": "eyJkb21haW4iOiJjcnlwdG9mb3J0dW5lLm1lbWVnby5haSJ9",
+      "signature": "wOunmrN2ClJNeGPHwxFnIDQH+uVOLgyv1+qVSNGEK4R+70jau3Ndt4K42NrvFTBrPex0A1+WD+IVAIiNkSAy9hw="
+    }
   };
 
   return NextResponse.json(farcasterConfig);
